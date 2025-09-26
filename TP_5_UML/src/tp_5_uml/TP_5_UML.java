5547264778c050b4d55c6a036393011ea49d3838
rickyroure <127377831+rickyroure@users.noreply.github.com>
2025-09-26 15:29:42 -0300

    Add files via upload

---
+++ TP_5_UML/src/tp_5_uml/TP_5_UML.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp_5_uml;
+
+/**
+ *
+ * @author 54351
+ */
+public class TP_5_UML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
