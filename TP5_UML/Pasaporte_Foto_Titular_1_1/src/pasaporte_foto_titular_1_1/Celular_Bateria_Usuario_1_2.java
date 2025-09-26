e6fd179fd7cc68040d240ad29884a263a245a7b8
rickyroure <127377831+rickyroure@users.noreply.github.com>
2025-09-26 14:10:28 -0300

    Add files via upload

---
+++ TP5_UML/Pasaporte_Foto_Titular_1_1/src/pasaporte_foto_titular_1_1/Celular_Bateria_Usuario_1_2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pasaporte_foto_titular_1_1;
+
+/**
+ *
+ * @author 54351
+ */
+public class Celular_Bateria_Usuario_1_2 {
+    
+}
