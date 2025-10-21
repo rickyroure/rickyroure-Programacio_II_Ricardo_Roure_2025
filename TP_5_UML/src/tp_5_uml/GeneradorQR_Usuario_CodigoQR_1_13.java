37d902cbc1189d3096034a62052c6d7b7465a49a
rickyroure <127377831+rickyroure@users.noreply.github.com>
2025-10-21 14:15:22 -0300

    Add files via upload

---
+++ TP_5_UML/src/tp_5_uml/GeneradorQR_Usuario_CodigoQR_1_13.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp_5_uml;
+
+/**
+ *
+ * @author 54351
+ */
+public class GeneradorQR_Usuario_CodigoQR_1_13 {
+    
+}
